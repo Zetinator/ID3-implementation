92ec6e2e5555147eb28371b2dbdb348a462e48db
Zetinator <erick_zetina@hotmail.com>
2018-05-21 22:59:02 +0200

    Class id3 added, fisher data example added.

---
+++ id3.m
@@ -0,0 +1,58 @@
+classdef id3
+  properties
+    fatherID;
+    ID;
+    featureToCheck;
+    classMark;
+    width;
+
+    descendant = [];
+
+    class;
+
+
+  end
+
+
+  methods
+    % constructor
+    function obj = id3(fatherID, ID, featureToCheck, classMark, width, class)
+        if nargin == 0
+          obj.fatherID = 0;
+          obj.ID = 0;
+          obj.featureToCheck = 0;
+          obj.classMark = 0;
+          obj.width = 0;
+          obj.class = 0;
+
+        elseif nargin == 6
+          obj.fatherID = fatherID;
+          obj.ID = ID;
+          obj.featureToCheck = featureToCheck;
+          obj.classMark = classMark;
+          obj.width = width;
+          obj.class = class;
+
+        else
+          error('Wrong number of input arguments')
+        end
+    end
+
+    function [success] = createTree(obj, data)
+      % When given the data matrix the "class" always in the first column... the rest of the columns are features...
+      featureLabels = [1:size(data,2)-1];
+      obj.descendant = [];
+      obj.descendant = [obj.descendant, pseudoID3_E(data,featureLabels,0,0,0,0)];
+
+      display('SUCCESS')
+    end
+
+    function [success] = test(obj)
+      % Just a test...
+      display('SUCCESS')
+    end
+
+  end
+
+
+end
--- pseudoID3_E.m
+++ pseudoID3_E.m
@@ -1,114 +1,128 @@
-function [tree] = pseudoID3(data,labels,nivel,labelFather,branch,brandOfTheCurse,greatness,tree)
-% ID3
-% In which level am I, of the recursive tree?
-nivel = nivel+1;
-% The class always in the first column...
-features = data(:,2:size(data,2));
-theClass = data(:,1);
+function [descendant] = pseudoID3_E(data,featureLabels,ID,featureToCheck,classMark,width)
+  display(data)
+  % ID3 code
+  % The class always in the first column... the rest of the columns are features...
+  features = data(:,2:size(data,2));
+  labels = data(:,1);
 
-% Probability of attribute Ak to have a value J
-if isempty(data)
-else
-    numOfClass = unique(theClass);
+  % terminal case
+  if isempty(data)
+  else
+    numOfClass = unique(labels);
     if size(numOfClass,1) == 1
-        tree(end+1,:) = [nivel,labelFather,branch,brandOfTheCurse,greatness,numOfClass]; 
-        
+      % leaf founded
+      strID = sprintf('%s', dicomuid);
+      descendant = [id3(ID, strID, featureToCheck, classMark, width, numOfClass)];
+      fprintf('node: %s found class: %d \n', descendant(1).ID, numOfClass);
     else
-        % Sturges rule
-        numInterval = ceil(1+log2(size(nonzeros(features(:,1)),1)));
-        amplitud = range(features)/(numInterval);
-        
-        % Number of features of value j in the range of the interval j;
-        for i=1:size(features,2)
-            [counts,centers] = hist(features(:,i),numInterval);
-            occurrences(i,:) = counts;
-            classMarks(:,i) = centers';
-        end
-        
-        % Transpose and normalize
-        probability = (1/size(features,1)) * occurrences';
-        
-        % Data per class (in submatrix)
-        for i = 1:size(data,1)
-            for j = 1:size(numOfClass,1)
-                if data(i,1) == numOfClass(j)
-                    subMat(i,:,j) = data(i,:);
-                end
-            end
-        end
-        
-        subM = subMat(:,2:size(subMat,2),:);
+      % Sturges rule to select a good number of intervals for the histogram.
+      numInterval = ceil(1+log2(size(nonzeros(features(:,1)),1)));
+      amplitud = range(features)/(numInterval);
 
-        % Probability of class Cj when Ak has a value of j
+      % Number of features of value j in the range of the interval j;
+      for i=1:size(features,2)
+        [counts,centers] = hist(features(:,i),numInterval);
+        % Should be an interval between  and numInterval??? ##Debug later
+        occurrences(i,:) = counts;
+        classMarks(:,i) = centers';
+      end
+
+      % Transpose and normalize to compute probability
+      probability = (1/size(features,1)) * occurrences';
+
+      % Data per class (in submatrix)
+      for i = 1:size(data,1)
         for j = 1:size(numOfClass,1)
-            for i=1:size(subM,2)
-                occurrencesPerClass(:,i,j) = hist(nonzeros(subM(:,i,j)),classMarks(:,i))';
-            end
-            probabilityPerClass(:,:,j) = occurrencesPerClass(:,:,j)/sum(occurrencesPerClass(:,1,j));
+          if data(i,1) == numOfClass(j)
+            % Maybe subMat(j,i,:) its more intuitive
+            subMat(i,:,j) = data(i,2:size(data,2));
+          end
         end
-        
-        % Entrophy of feature Ak, by Claude Shannon
-        for j = 1:size(features,2)
-            dEntrophy = 0;
-            for i = 1:numInterval
-                alfa = 0;
-                for k = 1:size(numOfClass,1)
-                    if probabilityPerClass(i,j,k) == 0
-                        aux = 1;
-                    else
-                        aux = probabilityPerClass(i,j,k);
-                    end
-                    alfa = probabilityPerClass(i,j,k) * log2(aux) + alfa;
-                end
-                dEntrophy = dEntrophy + probability(i,j) * (-1) * alfa;
-            end
-            entrophy(j) = dEntrophy;
+      end
+
+      % Probability of class Cj when feature Ak has a value of j
+      for j = 1:size(numOfClass,1)
+        for i=1:size(subMat,2)
+          occurrencesPerClass(:,i,j) = hist(nonzeros(subMat(:,i,j)),classMarks(:,i))';
         end
-        
+        probabilityPerClass(:,:,j) = occurrencesPerClass(:,:,j)/sum(occurrencesPerClass(:,1,j));
+      end
 
-        % Arrangement against case where the entropy is 0 is taken the attribute of the right...
-        if nnz(entrophy) == 0
-            pos = 1;
-
-        else
-            [minimum,pos] = min(entrophy);
-        end
-
-        % Label of father node...
-        labelFather = labels(pos);
-        cutLL = labels(1:pos-1);
-        cutLR = labels((pos+1):end);
-        labels = [cutLL,cutLR];
-
-        % New data...
-        cutIzq = data(:,1:pos);
-        cutDer = data(:,(pos+2):end);
-        data = [cutIzq,cutDer];
-        
-        % For each attribute of smaller entropy I take submatrices for the j ranges
-        for i = 1:size(data,1)
-            for j = 1:numInterval
-                epsilon = abs(features(i,pos) - classMarks(j,pos))/100;
-                if abs(features(i,pos) - classMarks(j,pos)) < (amplitud(pos)/2 + epsilon)
-                    subMatrixs(i,:,j) = data(i,:);
-                end
-            end
-        end
-        
-        % RecursivityTime
+      % Entrophy of feature Ak, by Claude Shannon
+      for j = 1:size(features,2)
+        dEntrophy = 0;
         for i = 1:numInterval
-            clear bringIt
-            for k = 1:size(features,2)
-                bringIt(:,k) = nonzeros(subMatrixs(:,k,i));
+          alfa = 0;
+          for k = 1:size(numOfClass,1)
+            if probabilityPerClass(i,j,k) == 0
+              aux = 1;
+            else
+              aux = probabilityPerClass(i,j,k);
             end
-            branch = i;
-            brandOfTheCurse = classMarks(i,pos);
-            greatness = amplitud(pos);
-            tree(end+1,:) = [nivel,labelFather,branch,brandOfTheCurse,greatness,0]; 
-            tree = pseudoID3(bringIt,labels,nivel,labelFather,branch,brandOfTheCurse,greatness,tree);
+            alfa = probabilityPerClass(i,j,k) * log2(aux) + alfa;
+          end
+          dEntrophy = dEntrophy + probability(i,j) * (-1) * alfa;
         end
-        
-    end
-end
-end
+        entrophy(j) = dEntrophy;
+      end
 
+
+      % Arrangement against case where the entropy is 0 is taken the attribute of the right...
+      if nnz(entrophy) == 0
+        pos = 1;
+
+      else
+        [minimum,pos] = min(entrophy);
+      end
+
+      % Label of father node would be the position index of the feature with min entropy...
+      featureToCheck = featureLabels(pos);
+
+      % prepare the new featureLabels that are going to be passed to the recursive children of the function
+      cutLL = featureLabels(1:pos-1);
+      cutLR = featureLabels((pos+1):end);
+      featureLabels = [cutLL,cutLR];
+
+      % New data that is going to be passed to the recursive children of the function...
+      cutIzq = data(:,1:pos);
+      cutDer = data(:,(pos+2):end);
+      data = [cutIzq,cutDer];
+
+      % For each attribute of the smaller entropy I take submatrices of dataFeatures for the "j" intervals to feed the "j" children
+      for i = 1:size(data,1)
+        for j = 1:numInterval
+          % relative epsilon to avoid floating point aritmetics mismatches
+          epsilon = abs(features(i,pos) - classMarks(j,pos))/100;
+          % check if the data belongs to the interval "j" using the distance to the class mark and range as threshold
+          if abs(features(i,pos) - classMarks(j,pos)) < (amplitud(pos)/2 + epsilon)
+            subMatrixs(i,:,j) = data(i,:);
+          end
+        end
+      end
+
+      strID = sprintf('%s', dicomuid);
+      node = id3(ID, strID, featureToCheck, classMark, width, 0);
+      for i = 1:numInterval
+        clear bringIt
+        auxCounter = 1;
+
+        % Clean the matrix of the zeros... can be debug later...
+        for k = 1:size(features,1)
+          if any(subMatrixs(k,:,i)')
+            bringIt(auxCounter,:) = subMatrixs(k,:,i);
+            auxCounter = auxCounter + 1;
+          end
+
+        end
+        if any(subMatrixs(:,:,i))
+          classMark = classMarks(i,pos);
+          width = amplitud(pos)/2;
+          % pushback in the descendant vector
+          node.descendant = [node.descendant, pseudoID3_E(bringIt,featureLabels,strID, featureToCheck,classMark,width)];
+        end
+      end
+      descendant = node;
+
+    end
+  end
+end
